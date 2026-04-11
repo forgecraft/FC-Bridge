0b5b7193edeb5806b32d45532093a67a6e085f64
Michael Hillcox <michaelhillcox@gmail.com>
2026-04-11 12:58:43 +0100

    fix: whoops

--- src/main/java/net/forgecraft/mods/bridge/client/ClientUtils.java
+++ src/main/java/net/forgecraft/mods/bridge/client/ClientUtils.java
@@ -1,10 +1,18 @@
 package net.forgecraft.mods.bridge.client;
 
+import com.mojang.logging.LogUtils;
+import net.forgecraft.mods.bridge.client.screens.ClientSettingsScreen;
+import net.forgecraft.mods.bridge.client.screens.TPSScreen;
+import net.forgecraft.mods.bridge.structs.ScreenType;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.Screen;
+import org.slf4j.Logger;
 
 import java.util.Optional;
 
 public class ClientUtils {
+    private static final Logger LOGGER = LogUtils.getLogger();
+
     /**
      * Sometimes you're gunna wanna try and get Minecraft before Minecraft is ready OR minecraft has finished setting up
      * this typically happens when you're doing something that happens right when minecraft loads but hasn't quite finished
@@ -18,5 +26,21 @@ public class ClientUtils {
         } catch (Exception e) {
             return Optional.empty();
         }
+    }
+
+    public static void showScreen(ScreenType type) {
+        Screen screen = null;
+        if (type == ScreenType.TPS) {
+            screen = new TPSScreen();
+        } else if (type == ScreenType.CLIENT_SETTINGS) {
+            screen = new ClientSettingsScreen();
+        }
+
+        if (screen == null) {
+            LOGGER.error("No screen found for type: {}", type);
+            return;
+        }
+
+        Minecraft.getInstance().setScreen(screen);
     }
 }
--- src/main/java/net/forgecraft/mods/bridge/client/network/ShowScreenReplyHandler.java
+++ src/main/java/net/forgecraft/mods/bridge/client/network/ShowScreenReplyHandler.java
@@ -1,5 +1,6 @@
 package net.forgecraft.mods.bridge.client.network;
 
+import net.forgecraft.mods.bridge.client.ClientUtils;
 import net.forgecraft.mods.bridge.client.screens.ClientSettingsScreen;
 import net.forgecraft.mods.bridge.client.screens.TPSScreen;
 import net.forgecraft.mods.bridge.structs.ScreenType;
@@ -11,25 +12,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ShowScreenReplyHandler {
-    private static final Logger LOGGER = LoggerFactory.getLogger(ShowScreenReplyHandler.class);
-
     public static void onClient(final ShowScreenPacket data, final IPayloadContext context) {
         context.enqueueWork(() -> {
             var type = data.screenType();
-
-            Screen screen = null;
-            if (type == ScreenType.TPS) {
-                screen = new TPSScreen();
-            } else if (type == ScreenType.CLIENT_SETTINGS) {
-                screen = new ClientSettingsScreen();
-            }
-
-            if (screen == null) {
-                LOGGER.error("No screen found for type: {}", type);
-                return;
-            }
-
-            Minecraft.getInstance().setScreen(screen);
+            ClientUtils.showScreen(type);
         });
     }
 }
